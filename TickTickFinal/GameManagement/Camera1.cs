99247a2af3d9ddef154e60e325ad15a12918ab05
Tony Nguyen <tonynguijen@gmail.com>
2020-10-30 10:52:29 +0100

    Alle files

---
+++ TickTickFinal/GameManagement/Camera1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+class Camera1
+{
+    private bool _constantSpeed;
+    private float _layer, _scrollingField, _speed;
+    //private List<(klasse waar sprites instaan?)> _sprites;
+
+    public float Layer
+    {
+        get { return _layer; }
+        set
+        {
+            _layer = value;
+
+            //foreach(var sprite in _sprites)
+            {
+                //sprite.Layer = _layer;
+            }
+        }
+    }
+}
+
---
+++ TickTickFinal/gameobjects/SpeedObject.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+class SpeedObject : SpriteGameObject
+{
+    float bounce;
+    public SpeedObject(int layer = 0, string id = "") : base("Sprites/...", layer, id)
+    {
+    }
+
+    public override void Update(GameTime gameTime)
+    {
+        double t = gameTime.TotalGameTime.TotalSeconds * 3.0f + Position.X;
+        bounce = (float)Math.Sin(t) * 0.2f;
+        position.Y += bounce;
+        Player player = GameWorld.Find("player") as Player;
+        if (visible && CollidesWith(player))
+        {
+            visible = false;
+            GameEnvironment.AssetManager.PlaySound("Sounds/snd_watercollected");
+        }
+    }
+}
